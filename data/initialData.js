636e0a38d0b42b09982ed485b4538b70805693a6
Keerthy <keerthyseshasayanam30@gmail.com>
2024-07-17 15:04:35 +0530

    first commit

---
+++ data/initialData.js
@@ -0,0 +1,124 @@
+const initialData = [
+    {
+      "bookName": "The Midnight Library",
+      "authorName": "Matt Haig",
+      "ISBN": 9780525559474,
+      "genre": "Fiction"
+    },
+    {
+      "bookName": "Where the Crawdads Sing",
+      "authorName": "Delia Owens",
+      "ISBN": 9780735219106,
+      "genre": "Mystery"
+    },
+    {
+      "bookName": "The Silent Patient",
+      "authorName": "Alex Michaelides",
+      "ISBN": 9781250301697,
+      "genre": "Thriller"
+    },
+    {
+      "bookName": "The Vanishing Half",
+      "authorName": "Brit Bennett",
+      "ISBN": 9780525536291,
+      "genre": "Historical Fiction"
+    },
+    {
+      "bookName": "The Book Thief",
+      "authorName": "Markus Zusak",
+      "ISBN": 9780375842206,
+      "genre": "Historical Fiction"
+    },
+    {
+      "bookName": "Dune",
+      "authorName": "Frank Herbert",
+      "ISBN": 9780441013593,
+      "genre": "Science Fiction"
+    },
+    {
+      "bookName": "1984",
+      "authorName": "George Orwell",
+      "ISBN": 9780451524935,
+      "genre": "Dystopian"
+    },
+    {
+      "bookName": "Brave New World",
+      "authorName": "Aldous Huxley",
+      "ISBN": 9780060850524,
+      "genre": "Dystopian"
+    },
+    {
+      "bookName": "Pride and Prejudice",
+      "authorName": "Jane Austen",
+      "ISBN": 9780141040349,
+      "genre": "Romance"
+    },
+    {
+      "bookName": "The Hobbit",
+      "authorName": "J.R.R. Tolkien",
+      "ISBN": 9780261103344,
+      "genre": "Fantasy"
+    },
+    {
+      "bookName": "The Night Circus",
+      "authorName": "Erin Morgenstern",
+      "ISBN": 9780307744432,
+      "genre": "Fantasy"
+    },
+    {
+      "bookName": "Circe",
+      "authorName": "Madeline Miller",
+      "ISBN": 9780316556341,
+      "genre": "Fantasy"
+    },
+    {
+      "bookName": "The Girl on the Train",
+      "authorName": "Paula Hawkins",
+      "ISBN": 9781594634024,
+      "genre": "Mystery"
+    },
+    {
+      "bookName": "Gone Girl",
+      "authorName": "Gillian Flynn",
+      "ISBN": 9780307588371,
+      "genre": "Thriller"
+    },
+    {
+      "bookName": "Big Little Lies",
+      "authorName": "Liane Moriarty",
+      "ISBN": 9780399167065,
+      "genre": "Fiction"
+    },
+    {
+      "bookName": "The Hunger Games",
+      "authorName": "Suzanne Collins",
+      "ISBN": 9780439023528,
+      "genre": "Dystopian"
+    },
+    {
+      "bookName": "Divergent",
+      "authorName": "Veronica Roth",
+      "ISBN": 9780062024039,
+      "genre": "Dystopian"
+    },
+    {
+      "bookName": "Red Rising",
+      "authorName": "Pierce Brown",
+      "ISBN": 9780345539786,
+      "genre": "Science Fiction"
+    },
+    {
+      "bookName": "The Martian",
+      "authorName": "Andy Weir",
+      "ISBN": 9780553418026,
+      "genre": "Science Fiction"
+    },
+    {
+      "bookName": "The Alchemist",
+      "authorName": "Paulo Coelho",
+      "ISBN": 9780061122415,
+      "genre": "Fiction"
+    }
+  ]
+  
+module.exports = initialData
